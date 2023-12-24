dde6695f6842fba0d9b1f139681b82c10bead101
yotsatorn <yotsatorn.pa@gmail.com>
2023-12-24 12:43:26 +0700

    feat: Implement POST, PUT, and DELETE operations for hero resource

--- labs/server/routes/heroes.ts
+++ labs/server/routes/heroes.ts
@@ -38,4 +38,77 @@ router.get("/:id", async (req: Request, res: Response) => {
   }
 });
 
+// POST /heroes
+router.post("/", async (req: Request, res: Response) => {
+  try {
+    // ดึงข้อมูลฮีโร่ใหม่จากข้อมูลที่ส่งมาในร้องขอ
+    const newHeroData: IHero = req.body;
+
+    // สร้างฮีโร่ใหม่โดยใช้ HeroModel
+    const newHero = new HeroModel(newHeroData);
+
+    // บันทึกฮีโร่ใหม่ลงในฐานข้อมูล
+    await newHero.save();
+
+    res.status(201).json({ 
+      message: "insert successfully",
+      newHero,
+     });
+  } catch (error: any) {
+    console.error("Error creating hero (เกิดข้อผิดพลาดในการสร้างฮีโร่) :", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// PUT /heroes/:id
+router.put("/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    // ค้นหาฮีโร่ตาม ID
+    const existingHero: IHero | null = await HeroModel.findById(id);
+
+    if (!existingHero) {
+      return res.status(404).json({ message: "Hero not found (ไม่พบฮีโร่)" });
+    }
+
+    // อัปเดตฮีโร่ที่มีอยู่ด้วยข้อมูลใหม่
+    Object.assign(existingHero, req.body);
+
+    // บันทึกฮีโร่ที่อัปเดตลงในฐานข้อมูล
+    await existingHero.save();
+
+    res.json({ 
+      message: "update successfully",
+      existingHero,
+     });
+  } catch (error: any) {
+    console.error("Error updating hero (เกิดข้อผิดพลาดในการอัปเดตฮีโร่):", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// DELETE /heroes/:id
+router.delete("/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    // ค้นหาฮีโร่ตาม ID และลบ
+    const deletedHero: IHero | null = await HeroModel.findByIdAndDelete(id) as unknown as IHero;
+
+    if (deletedHero) {
+      res.json({ 
+        message: "delete successfully",
+        deletedHero,
+       });
+    } else {
+      res.status(404).json({ message: "Hero not found (ไม่พบฮีโร่)" });
+    }
+  } catch (error: any) {
+    console.error("Error deleting hero (เกิดข้อผิดพลาดในการลบฮีโร่):", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
 export default router;
