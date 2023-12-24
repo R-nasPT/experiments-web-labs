810da06d28e33c1c67cfdadb65e58eb479d3022f
yotsatorn <yotsatorn.pa@gmail.com>
2023-12-24 20:44:12 +0700

    setup project front end

---
+++ labs/client/tanstack-query/src/pages/Homepage.tsx
@@ -0,0 +1,11 @@
+import cap from '../assets/captain-america.ico'
+
+export default function Homepage() {
+  return (
+    <div>
+      <h1 className='text-red-500 text-9xl'>Homepage</h1>
+      {/* <img src={`../assets/captain-america.ico`} alt="" /> */}
+      <img src={cap} alt="" />
+    </div>
+  );
+}
