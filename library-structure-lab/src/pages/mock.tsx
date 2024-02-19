b78b9b898f3c6a288d05e3b40bda30c64c235f98
Yotsatorn  Pathan <120444604+R-nasPT@users.noreply.github.com>
2024-02-19 09:31:46 +0700

    add layout container

--- library-structure-lab/src/pages/mock.tsx
+++ library-structure-lab/src/pages/mock.tsx
@@ -5,7 +5,7 @@ export default function Mock() {
   const [toggle, setToggle] = useToggle(false);
   return (
     <>
-      <div>
+      <div className="grid grid-cols-2 max-w-screen- w-full mx-auto"> {/* max-w-screen-sm w-full mx-auto <--นี่คือ container ที่คล้ายกับ MUI */}
         <button>click toggle</button>
       </div>
       {toggle && <PopupComfirm handleClose={() => setToggle()} />}
