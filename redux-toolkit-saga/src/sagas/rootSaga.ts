8ddb76768dd0140f9edebda5e749057cbe65c85a
Yotsatorn  Pathan <120444604+R-nasPT@users.noreply.github.com>
2024-09-20 18:51:31 +0700

    Delete redux-toolkit-saga directory

--- redux-toolkit-saga/src/sagas/rootSaga.ts
+++
@@ -1,9 +0,0 @@
-import { all } from "redux-saga/effects";
-import { watchFetchData } from "./apiSaga";
-
-export default function* rootSaga() {
-  yield all([
-    watchFetchData(),
-    // เพิ่ม Sagas อื่น ๆ ตรงนี้ (ถ้ามี)
-  ]);
-}
--- redux-toolkit-saga/src/services/baseApi.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const baseApi = axios.create({
-  baseURL: "http://localhost:2077",
-  // timeout: 1000, // กำหนด timeout สำหรับการเรียก API
-});
-
-export default baseApi;
